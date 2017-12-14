bd690172fcbd7a8d516ea3bfd467bf503f0bcbba
ken.hurtado <ken.hurtado@icarus.com>
2017-12-13 17:44:30 -0700

    First commit for EscapeRoom project

--- EscapeRoom/index.js
+++ EscapeRoom/index.js
@@ -1,13 +1,31 @@
 const rs = require("readline-sync");
 
-console.log("Youa are in a locked room. \nThere is a door, a hidden key, and a hole in the wall.");
+console.log("You are in a locked room.\nThere is a door, a hidden key and a hole in the wall.");
 
-let userInput = rs.question("What would you like to do?");
+let inv = [];
+let isAlive = true;
 
+while(isAlive) {
+    let userinput = rs.question("What would you like to do? ");
+    userinput = userinput.toLowerCase();
 
-
-//possible things do to in room
-//look -> will print a description of the room
-//open door -> try to open door (if you have key go out if not you FAIL!)
-//put hand in the hole wall -> they die (GAME OVER)
-//take key -> put key in inventory
+    if(userinput.includes("hole") && userinput.includes("put")) {
+        console.log("Sorry... um ... you died");
+        isAlive = false;
+        console.log("THE CAKE IS A LIE");
+    } else if(userinput.includes("open") && userinput.includes("door")) {
+        if(inv.includes("key")) {
+            console.log("Congrats you WIN the game!! Yo'ur going to Disneyland!!");
+            break;
+        } else {
+            console.log("Sorry you dont have the key you idiot.");
+        }
+    } else if(userinput.includes("take") && userinput.includes("key")) {
+        console.log("You found the key but is it the key?");
+        inv.push("key");
+    } else if(userinput.includes("look")) {
+        console.log("You are in a locked room.\nThere is a door, a hidden key and a hole in the wall.");        
+    } else {
+        console.log("Yo'ur idiot? What are you new? That isnt one of the commands");
+    }
+}
