c8349868b79200f1e96fd230181cd40036abcf24
ken.hurtado <ken.hurtado@icarus.com>
2017-12-22 17:00:53 -0700

    Moved BusinessTime and Mario Pest

---
+++ BusinessTime/index.js
@@ -0,0 +1,87 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+
+/******************************/
+
+
+$(function() {
+    function after_form_submitted(data) {
+        if (data.result == 'success') {
+            $('form#reused_form').hide();
+            $('#success_message').show();
+            $('#error_message').hide();
+        } else {
+            $('#error_message').append('<ul></ul>');
+
+            jQuery.each(data.errors, function(key, val) {
+                $('#error_message ul').append('<li>' + key + ':' + val + '</li>');
+            });
+            $('#success_message').hide();
+            $('#error_message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function() {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if (label) {
+                    $btn.prop('type', 'submit');
+                    $btn.text(label);
+                    $btn.prop('orig_label', '');
+                }
+            });
+
+        } //else
+    }
+
+    $('#reused_form').submit(function(e) {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function() {
+            $btn = $(this);
+            $btn.prop('type', 'button');
+            $btn.prop('orig_label', $btn.text());
+            $btn.text('Sending ...');
+        });
+
+
+        $.ajax({
+            type: "POST",
+            url: 'handler.php',
+            data: $form.serialize(),
+            success: after_form_submitted,
+            dataType: 'json'
+        });
+
+    });
+});
