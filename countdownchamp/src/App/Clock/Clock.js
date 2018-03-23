16a3808e83e42ee4d2a55347755a42d126f495b8
ken.hurtado <khurtado@protonmail.com>
2018-03-23 17:49:24 -0600

    Added getTimeUntil method to calculate time left from the past deadline to the current time.

--- countdownchamp/src/App/Clock/Clock.js
+++ countdownchamp/src/App/Clock/Clock.js
@@ -1,30 +1,45 @@
-import React, { Component } from 'react'
+// eslint-disable-next-line
+import React, { Component } from 'react';
 
 import './Clock.css';
 
 export default class Clock extends Component {
-    constructor(props) { // For object oriented inherited programming style of ES6 JavaScript
-        super(props);
-        // Declare initial state
-        this.state = {
-            day: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0
-        }
-        console.log('this.props', this.props)
+  constructor(props) { // For object oriented inherited programming style of ES6 JavaScript
+    super(props);
+    // Declare initial state
+    this.state = {
+      day: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
+  getTimeUntil(deadline) {
+        /* The 'new Date' grabs current time and sets that time as its time.
+        so the subtraction calculation is subtracting the current time
+        between the deadline and the current date */
+      const time = Date.parse(deadline) - Date.parse(new Date());
+        /*  */
+      const seconds = Math.floor((time/1000)%60);
+      const minutes = Math.floor((time/1000/60)%60);
+      const hours = Math.floor(time/(1000*60)*60)%24;
+      const days = Math.floor(time/(1000*60*60*24));
     }
 
-    render() {
-        return (
+  render() {
+    return (
             <div>
                 <div>
-                    <div className="Clock-days" >{this.state.days} days</div>
-                    <div className="Clock-hours" >{this.state.hours} hours</div>
-                    <div className="Clock-minutes" >{this.state.minutes} miuntes</div>
-                    <div className="Clock-seconds" >{this.state.seconds} seconds</div>
+                    <div className="Clock-days" >{ this.state.days } days</div>
+                    <div className="Clock-hours" >{ this.state.hours } hours</div>
+                    <div className="Clock-minutes" >{ this.state.minutes } miuntes</div>
+                    <div className="Clock-seconds" >{ this.state.seconds } seconds</div>
                 </div>
             </div>
-        )
-    }
+    );
+  }
 }
+
+// stopped at 5:10
+// https://www.udemy.com/react-js-and-redux-mastering-web-apps/learn/v4/t/lecture/6378964?start=0
