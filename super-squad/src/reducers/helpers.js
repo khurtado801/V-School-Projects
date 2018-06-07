0a74880ce6a4193dec68aa44117e162e6f090564
ken.hurtado <khurtado@protonmail.com>
2018-06-07 13:35:42 -0600

    Redo.

--- super-squad/src/reducers/helpers.js
+++
@@ -1,7 +0,0 @@
-import characters_json from '../data/characters.json';
-
-// createCharacter reducer
-export function createCharacter(id) {
-  let character = characters_json.find(c => c.id === id);
-  return character;
-}
